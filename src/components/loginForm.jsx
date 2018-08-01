4be80775006f6ec58a80d38f5f0c6117ca9f300d
Runbo Zhao <rzhao03@syr.edu>
2018-07-31 21:52:11 -0400

    Controled element.

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  state = {
+    account: { username: "", password: "" }
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    console.log("Submit");
+  };
+
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
+
   render() {
     return (
       <div>
         <h1>Login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={this.state.account.username}
+              onChange={this.handleChange}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
