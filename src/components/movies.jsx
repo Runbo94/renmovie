7df7bfca00f6d286214998674c127e9786d3da97
Runbo Zhao <rzhao03@syr.edu>
2018-08-11 18:28:55 -0400

    Replacing fake movie service.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
+import { toast } from "react-toastify";
 import React, { Component } from "react";
-import { getMovies } from "../services/fakeMovieService";
+import { getMovies, deleteMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
@@ -22,12 +23,24 @@ class Movies extends Component {
   async componentDidMount() {
     const { data } = await getGenres();
     const genres = [{ _id: "", name: "All Genres" }, ...data];
-    this.setState({ movies: getMovies(), genres });
+
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
-  handleDelete = movie => {
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
+  handleDelete = async movie => {
+    const originalMovies = this.state.movies;
+    const movies = originalMovies.filter(m => m._id !== movie._id);
     this.setState({ movies });
+
+    try {
+      await deleteMovie(movie._id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error("This movie has already been deleted!");
+
+      this.setState({ movies: originalMovies });
+    }
   };
 
   handleLike = movie => {
