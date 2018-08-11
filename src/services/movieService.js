c01f1a11b61062d95c1df8a4a46b0ef3bb6b172b
Runbo Zhao <rzhao03@syr.edu>
2018-08-11 19:28:45 -0400

    Saving the movies.

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -9,7 +9,15 @@ export function getMovie(movieId) {
   return http.get(`${config.apiEndpoint}/movies/${movieId}`);
 }
 
-export function saveMovie(movie) {}
+export function saveMovie(movie) {
+  if (movie._id) {
+    const body = { ...movie };
+    delete body._id;
+    return http.put(`${config.apiEndpoint}/movies/${movie._id}`, body);
+  }
+
+  return http.post(`${config.apiEndpoint}/movies`, movie);
+}
 
 export function deleteMovie(movieId) {
   return http.delete(`${config.apiEndpoint}/movies/${movieId}`);
