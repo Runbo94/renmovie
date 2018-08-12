57c1470536526ba9c8487f95da8180d93e4ec63e
Runbo Zhao <rzhao03@syr.edu>
2018-08-12 19:13:29 -0400

    111

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import { login } from "../services/authService";
+import auth, { login } from "../services/authService";
 
 class LoginForm extends Form {
   state = {
@@ -22,7 +23,9 @@ class LoginForm extends Form {
     try {
       const { data } = this.state;
       await login(data.username, data.password);
-      window.location = "/";
+
+      const { state } = this.props.location;
+      window.location = state ? state.from.pathname : "/";
     } catch (ex) {
       if (ex.response && ex.response.status === 400) {
         const errors = { ...this.state.errors };
@@ -33,6 +36,8 @@ class LoginForm extends Form {
   };
 
   render() {
+    if (auth.getCurrentUser()) return <Redirect to="/" />;
+
     return (
       <div>
         <h1>Login</h1>
