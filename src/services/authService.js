26fb11ab2b2aee30cbce4c3588855c9ebd1d7005
Runbo Zhao <rzhao03@syr.edu>
2018-08-12 12:40:07 -0400

    Refactorying.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,34 @@
 import http from "./httpService";
+import jwtDecode from "jwt-decode";
 import { apiEndpoint } from "../config.json";
 
 const api = apiEndpoint + "/auth";
 
-export function login(email, password) {
-  return http.post(api, { email, password });
+export async function login(email, password) {
+  const { data: jwt } = await http.post(api, { email, password });
+  localStorage.setItem("token", jwt);
 }
+
+export function loginWithJwt(jwt) {
+  localStorage.setItem("token", jwt);
+}
+
+export function logout() {
+  localStorage.removeItem("token");
+}
+
+export function getCurrentUser() {
+  try {
+    const jwt = localStorage.getItem("token");
+    return jwtDecode(jwt);
+  } catch (ex) {
+    return null;
+  }
+}
+
+export default {
+  login,
+  logout,
+  getCurrentUser,
+  loginWithJwt
+};
